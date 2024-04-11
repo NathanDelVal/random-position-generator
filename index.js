768fe274082dcaa9577fa490638f505f3724c605
NathanMad <nathan@internit.com.br>
2024-04-11 14:09:33 -0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('button').addEventListener('mouseover', (e) => {
 
-        let rndmY = Math.random() * window.screen.height - e.target.offsetHeight;
-        let rndmX = Math.random() * window.screen.width - e.target.offsetWidth;
+        let rndmY = Math.random() * window.screen.availHeight - e.target.offsetHeight;
+        let rndmX = Math.random() * window.screen.availWidth - e.target.offsetWidth;
 
         e.target.style.position = 'absolute';
         e.target.style.top = `${rndmY}px`;
