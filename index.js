afee3b11e44c00d8d4433bc33baca9aa7f5bc3d3
NathanMad <nathan@internit.com.br>
2024-04-14 11:51:55 -0300

    deixando mais generico

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('button').addEventListener('mouseover', (e) => {
 
-        let rndmY = Math.random() * window.screen.availHeight - e.target.offsetHeight;
-        let rndmX = Math.random() * window.screen.availWidth - e.target.offsetWidth;
+        let rndmY = Math.random() * e.target.closest('[data-rndm-target]').offsetHeight - e.target.offsetHeight + 20;
+        let rndmX = Math.random() * e.target.closest('[data-rndm-target]').offsetWidth - e.target.offsetWidth + 20;
 
         e.target.style.position = 'absolute';
         e.target.style.top = `${rndmY}px`;
